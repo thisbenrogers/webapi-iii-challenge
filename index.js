de23ddb03909c0b66e0ba01e5caf429feccd285e
Ben <thisbenrogers@gmail.com>
2019-10-31 17:09:42 -0500

    :sparkles: Adds logger middleware

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require('./server');
 
-const port = 5432;
+const port = 5000;
 
 server.listen(port, () => {
   console.log(`\n<<< Service running on port ${port} >>>\n`);
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 
+const logger = require('./middleware/logger');
+
 const server = express();
 
+server.use(logger);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.status(200).json({ message: "It's alive!" })
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
